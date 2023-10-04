521e8791036f4b93ec76d11ea94f1dbac1b313c1
bhazalb <bayramhazal030@gmail.com>
2023-10-04 16:46:11 +0300

    cc

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <myComp />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainContainer from "@/components/MainContainer"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    myComp: MainContainer
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <p> {{ text }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+       text: "Lorem jyhtgrefegrhtgrefghtrgewfegrefwegrefwegrhrgetwfegrefgrhtrgegrhj"
+        }
+    }
+    methods: {
+    },
+    computed : {},
+    watch: {}
+}
+</script>
+<style >
+    
+</style>
